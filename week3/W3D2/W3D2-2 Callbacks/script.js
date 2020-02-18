8dd94b9b00c0e4299cb0d73652da72514e7fba4c
judithbloem <judith@Judith-prive.local>
2020-02-18 11:46:53 +0100

    aanpassingen mapnamen

--- week3/W3D2/W3D2-2 Callbacks/script.js
+++ week3/W3D2/W3D2-2 Callbacks/script.js
@@ -1,11 +1,11 @@
 const huiswerkMaken = (vak, callBack) => {
     console.log(`"Ok,ok, ik ga nu mijn ${vak} huiswerk maken"`);
-    setTimeout = () => {callback(); }, 3000;
+    setTimeout = () => {callback(); },5000;
 }
 const klaarMetHuiswerk = () => {
     console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!")
 }
-huiswerkMaken("wiskunde", klaarMetHuiswerk);
+huiswerkMaken("wiskunde", klaarMetHuiswerk());
 
 
 // const huiswerkMaken = function(vak, callback) {
